82692544f44bad7f71ff57109c08a6903a0026e7
raouf <raoufboukhobza3@gmail.com>
2025-03-23 01:14:51 +0100

    create migrations to add some tables to db

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('userName')->unique();
+            $table->foreignId('employe_id')->references('id')->on('employes')->onDelete('cascade');
+            $table->enum('role' ,['Gérant', 'Serveur', 'Caissier', 'Cuisinier']);
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
