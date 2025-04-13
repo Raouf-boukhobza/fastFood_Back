d9138c2a399d9d55241c1ee103e22dda07a15b2e
raouf <raoufboukhobza3@gmail.com>
2025-04-13 21:59:59 +0100

    create the salaries table

---
+++ database/migrations/2025_04_13_205450_create_salaries_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('salaries', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('employe_id')-> references('id')->on('employes')->onDelete('cascade');
+            $table->decimal('amount', 10, 2);
+            $table->date('payment_date');
+            $table->enum('status', ['paid', 'pending'])->default('pending');
+            $table->string('payment_method')->nullable();
+            $table->decimal('hours_worked', 10, 2)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('salaries');
+    }
+};
